56aafc94d60704cd0ad268ba3bfcc748588a13e1
Tran Van Quyet <55977448+QuyetTV1991@users.noreply.github.com>
2023-09-28 08:15:53 +0000

    break down page into smaller

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7%e+9!%-@4f-iad1+4=5h@3y@5k&o9!&j&leh+5h8&5%9x9w6e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -52,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "myProject.urls"
 
+import os
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

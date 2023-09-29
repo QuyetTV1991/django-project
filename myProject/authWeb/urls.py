ea1b769013856295d30a11248c0fe3f93f8421cf
Tran Van Quyet <55977448+QuyetTV1991@users.noreply.github.com>
2023-09-29 08:59:48 +0000

    signup - login - logout >> done

--- myProject/authWeb/urls.py
+++ myProject/authWeb/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.login, name="login"),
+    path("login/", views.handledLogin, name="handledLogin"),
     path("signup/", views.signup, name="signup"),
-    path("logout/", views.logout, name="logout"),
+    path("logout/", views.handledLogout, name="handledLogout"),
+    path("activate/<uidb64>/<token>", views.ActivateAccountView.as_view(), name='activate')
 ]
--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "six",
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,15 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
+
+# Sending emails
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'quinlan.ecommerce@gmail.com'
+EMAIL_HOST_PASSWORD = 'cwlfjsjhuxtvaqvw'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 # Static files (CSS, JavaScript, Images)
